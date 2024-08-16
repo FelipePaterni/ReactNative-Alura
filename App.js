810452f7b5319f7bfdebef01812c80ef446a1259
FelipePaterni <118012333+FelipePaterni@users.noreply.github.com>
2024-08-16 07:54:04 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,28 +1,49 @@
 import React, { Component } from 'react';
-import { View, ScrollView, Text, StyleSheet } from 'react-native';
+import { View, ScrollView, Text, StyleSheet, FlatList } from 'react-native';
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      feed: [
+        { id: '1', nome: 'xxxx', idade: 21, email: 'xxxx@etec.sp.gov.br' },
+        { id: '2', nome: 'xxxx', idade: 15, email: 'xxxx@etec.sp.gov.br' },
+        { id: '3', nome: 'xxxx', idade: 40, email: 'xxxx@etec.sp.gov.br' },
+        { id: '4', nome: 'xxxx', idade: 50, email: 'xxxx@etec.sp.gov.br' },
+      ]
+    };
+  }
+
+
   render() {
     return (
       <View style={style.container}>
 
-        <ScrollView showsVerticalScrollIndicator={false}>
-          <View style={style.box1}></View>
-          <View style={style.box2}></View>
-          <View style={style.box3}></View>
-          <View style={style.box4}></View>
-        </ScrollView>
+        <FlatList
+          data={this.state.feed}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <Dados data={item} />}
+        />
 
-    
-        <ScrollView Vert={true}>
-          <View style={style.box1H}></View>
-          <View style={style.box2H}></View>
-          <View style={style.box3H}></View>
-          <View style={style.box4H}></View>
-        </ScrollView>
+
 
       </View>
+    );
+  }
+}
+
+class Dados extends Component {
+  render() {
+    return (
+
+      <View style={style.areaDados}>
+        <Text style={style.textoDados}>ID: {this.props.data.id}</Text>
+        <Text style={style.textoDados}>Nome:  {this.props.data.nome} </Text>
+        <Text style={style.textoDados}>Idade: {this.props.data.idade}</Text>
+        <Text style={style.textoDados}>Email:  {this.props.data.email}</Text>
+      </View>
+
     );
   }
 }
@@ -30,6 +51,20 @@ class App extends Component {
 const style = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 15,
+    margin: 15,
+    paddingTop:40,
+
+  },
+  areaDados: {
+    backgroundColor: "#222",
+    height: 200,
+    marginBottom: 15
+  },
+  textoDados: {
+    color: "#fff",
+    fontSize: 20,
+    padding: 10,
   },
   box1: {
     backgroundColor: 'red',
