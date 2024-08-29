f64a51d58f1bb21cd3867a395b7b34e15e85228c
FelipePaterni <118012333+FelipePaterni@users.noreply.github.com>
2024-08-29 16:31:04 -0300

    aaaaaa

--- App.js
+++ App.js
@@ -1,22 +1,39 @@
 import React, { Component } from 'react';
 import {
-View,
-Text,
-StyleSheet,
+  View,
+  Text,
+  StyleSheet,
 } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
 
 export default class App extends Component {
-render(){
-return (
- <View style={styles.container}>
-<Text style={styles.titulo}>------------Menu ------</Text>
-
-
-
-
-</View>
-);
-}
+  constructor(props) {
+    super(props);
+    this.state = {
+      menu: 0,
+      lists: [
+        { key: 1, nome: "        ", valor: 0 },
+        { key: 2, nome: "Roupa de cama", valor: 59 },
+        { key: 3, nome: "Roupa de banho", valor: 37 },
+        { key: 4, nome: "Eletro/Eletronico", valor: 25.70 },
+        { key: 5, nome: "Geral", valor: 70 },
+      ]
+    };
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.titulo}>------------Menu------------</Text>
+        <Picker selectedValue={this.state.menu}
+          onValueChange={(itemValue, itemIndex) => this.setState({ menu: itemValue })}>
+          <Picker.Item key={1} value={1} label={"     "} />
+          <Picker.Item key={2} value={2} label={"Roupa de Cama"} />
+          <Picker.Item key={3} value={3} label={"Roupa de Banho"} />
+          <Picker.Item key={4} value={4} label={"Setor de Eletrônicos"} />
+        </Picker>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
